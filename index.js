f7311d2c53e56dbfb2c69102dcf6ee3db97aed23
Nicklaus Liow <nicklaus.liow@gmail.com>
2013-10-23 16:14:38 -0700

    update server code

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 var connect = require('connect');
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 connect.createServer(connect.static(__dirname+'/build')).listen(port);
+console.log("Server started on port "+port);
