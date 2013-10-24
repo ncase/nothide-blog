f81ed6dfd0502c638244e5fa504a17588d967b64
Nicklaus Liow <nicklaus.liow@gmail.com>
2013-10-23 18:18:20 -0700

    Social sharing that doesn't track'

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# The NCASE blog
+# The Nothing to Hide blog
 
-For my personal site, [ncase.me](http://www.ncase.me).    
 All contents of this blog are dedicated to the public domain, using the [Creative Commons Zero](http://creativecommons.org/publicdomain/zero/1.0/) license.
 
 Built with [wintersmith](https://github.com/jnordberg/wintersmith).    
--- index.js
+++ index.js
@@ -1,41 +1,34 @@
 #!/bin/env node
-//  OpenShift sample Node application
 var express = require('express');
-var fs      = require('fs');
+var MyApp = function() {
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+	var self = this;
 
-    //  Scope.
-    var self = this;
+	// Initialize
+    self.initialize = function() {
+        self.setupVariables();
+        self.setupTerminationHandlers();
+        self.app = express.createServer();
+        self.app.use(express.static(__dirname+'/build'));
+    };
+    self.start = function() {
+        self.app.listen(self.port, self.ipaddress, function() {
+            console.log('%s: Node server started on %s:%d ...', Date(Date.now()), self.ipaddress, self.port);
+        });
+    };
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
-
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
+	// Setup Vars
     self.setupVariables = function() {
-        //  Set the environment variables we need.
         self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
+        self.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+        if(typeof self.ipaddress === "undefined"){
+            // Run locally
             console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
             self.ipaddress = "127.0.0.1";
         };
     };
 
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
+    // Terminate
     self.terminator = function(sig){
         if (typeof sig === "string") {
            console.log('%s: Received %s - terminating sample app ...',
@@ -44,56 +37,18 @@ var SampleApp = function() {
         }
         console.log('%s: Node server stopped.', Date(Date.now()) );
     };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
     self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
         process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
         ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
          'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
         ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.setupTerminationHandlers();
-        self.app = express.createServer();
-        self.app.use(express.static(__dirname+'/build'));
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...', Date(Date.now()), self.ipaddress, self.port);
+            process.on(element, function(){ self.terminator(element); });
         });
     };
 
 };
 
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
+// Main(); code
+var app = new MyApp();
+app.initialize();
+app.start();
